f53db7dd7af7b7f47d96028e7d72340114c45700
Łukasz Rutkowski <lukus178@gmail.com>
2018-04-18 17:20:26 +0200

    Colorize calendar checkboxes

--- app/src/main/java/me/thanel/linecalendar/calendar/CalendarAdapter.kt
+++ app/src/main/java/me/thanel/linecalendar/calendar/CalendarAdapter.kt
@@ -1,16 +1,16 @@
 package me.thanel.linecalendar.calendar
 
+import android.content.res.ColorStateList
+import android.support.v4.widget.CompoundButtonCompat
 import android.support.v7.recyclerview.extensions.ListAdapter
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
-import android.widget.ImageView
 import android.widget.TextView
 import kotlinx.android.synthetic.main.item_calendar.view.*
 import me.thanel.linecalendar.R
-import me.thanel.linecalendar.util.tintDrawable
 
 class CalendarAdapter :
     ListAdapter<CalendarData, CalendarAdapter.ViewHolder>(CalendarData.DIFF_CALLBACK) {
@@ -23,10 +23,13 @@ class CalendarAdapter :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val calendarData = getItem(position)
-        holder.calendarIcon.tintDrawable(calendarData.color)
         holder.calendarData = calendarData
         holder.calendarNameView.text = calendarData.name
         holder.calendarCheckBox.isChecked = calendarData.isChecked
+        CompoundButtonCompat.setButtonTintList(
+            holder.calendarCheckBox,
+            ColorStateList.valueOf(calendarData.color)
+        )
     }
 
     fun getSelectedCalendars(): Set<Long> = (0 until itemCount)
@@ -35,7 +38,6 @@ class CalendarAdapter :
         .mapTo(mutableSetOf()) { it.id }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val calendarIcon: ImageView = itemView.calendarIcon
         val calendarNameView: TextView = itemView.calendarNameView
         val calendarCheckBox: CheckBox = itemView.calendarCheckBox
 
